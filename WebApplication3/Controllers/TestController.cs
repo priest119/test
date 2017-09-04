02dce92eea9783af318c8d198187ae8554bb4402
priest119 <priest119@qq.com>
2017-09-04 14:25:26 +0800

    测试提交

---
+++ WebApplication3/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication3.Controllers
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class TestController : ApiController
+    {
+        [HttpPost]
+        public string fuck([FromBody]string name) {
+            return "老子love"+name;
+        }
+    }
+}
